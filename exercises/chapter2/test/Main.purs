9514f26f0e38e2824b096cbc7f19f0cd4799e7c4
Woonki Moon <woonki.moon@gmail.com>
2021-04-09 10:56:55 +0900

    챕터2 연습문제 풀이

--- exercises/chapter2/test/Main.purs
+++ exercises/chapter2/test/Main.purs
@@ -16,7 +16,6 @@ main = do
         Assert.equal 23 (answer 10)
       test "below 1000" do
         Assert.equal 233168 (answer 1000)
-    {-  Move this block comment starting point to enable more tests
     suite "diagonal" do
       test "3 4 5" do
         Assert.equal 5.0 (diagonal 3.0 4.0)
@@ -34,5 +33,6 @@ main = do
         Assert.equal 56 (leftoverCents 456)
       test "-789" do
         Assert.equal (-89) (leftoverCents (-789))
+    {-  Move this block comment starting point to enable more tests
 
 -}
--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,16 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Data.Int (rem, toNumber)
+import Math (pi, pow, sqrt)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = (*) pi $ pow r $ toNumber 2
+-- Int는 Number 타입 클래스를 만족하지 않는가?
+
+leftoverCents :: Int -> Int
+leftoverCents n = rem n 100
