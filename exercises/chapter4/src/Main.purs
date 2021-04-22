9a04168d787b9cc879f58428021d0b6182918477
Woonki Moon <woonki.moon@gmail.com>
2021-04-22 17:38:25 +0900

    콘솔에 직접 출력해보기

--- exercises/chapter4/src/Main.purs
+++ exercises/chapter4/src/Main.purs
@@ -2,9 +2,16 @@ module Main where
 
 import Prelude
 
+import Data.List (List(..), (:))
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console (logShow)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  logShow $ isSameList isSame (1 : 2 : 3 : Nil) ("1" : "2" : "3" : Nil)
+
+isSame :: Int -> String -> Boolean
+isSame n s = eq s $ show n
+
+isSameList :: (Int -> String -> Boolean) -> List Int -> List String -> List Boolean
+isSameList f ln ls = isSame <$> ln <*> ls
